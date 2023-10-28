bf762a4d0a104f2e809bf9ad4e1d61dbd4f7d9c9
Megan <m_acosta4@u.pacific.edu>
2023-10-28 11:10:58 -0700

    different batch tests

--- hume_tutorials/batch_test.py
+++ hume_tutorials/batch_test.py
@@ -1,5 +1,6 @@
 from hume import HumeBatchClient
 from hume.models.config import ProsodyConfig # State of speech
+from hume.models.config import FaceConfig # Facial expression
 import os
 import json
 import requests
@@ -20,7 +21,25 @@ def first_test():
     print(job_id)
 
 
-def test_methods():
+def multi_test_method():
+    client = HumeBatchClient(API_KEY)
+    filepaths = ["clips.zip"]
+    config = [FaceConfig(identify_faces=True), ProsodyConfig()]
+    job = client.submit_job([], config, files=filepaths)
+
+    print("Running...", job)
+
+    job.await_complete()
+    print(job.get_predictions())
+
+    print("Saving predictions to predictions.json")
+    job.download_predictions("predictions.json")
+
+    print("Artifacts download to artifacts.zip")
+    job.download_artifacts("artifacts.zip")
+
+
+def single_test_method():
     client = HumeBatchClient(API_KEY)
     file = ["audio_clip.mp3"]
     config = [ProsodyConfig()]
@@ -29,8 +48,23 @@ def test_methods():
     print("Running...", job)
 
     job.await_complete()
-    print(job.get_predictions())
+    #print(job.get_predictions())
+    print("Saving predictions to speech_pred.json")
+    job.download_predictions("speech_pred.json")
+
+    print("Artifacts download to speech_artifacts.zip")
+    job.download_artifacts("speech_artifacts.zip")
 
 
-test_methods()
+def check_job():
+    client = HumeBatchClient(API_KEY)
+    job = client.get_job('a6006ada-3ae0-4c18-9792-af0e2be5cbd5')
+    status = job.get_status()
+    print(status)
+
+
+
+#multi_test_method()
+single_test_method()
 #main()
+#check_job()
