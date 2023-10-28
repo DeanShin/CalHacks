1a60603e8756e5b185544a0f92403d84e0acc336
Dean Shin <deanshin365@gmail.com>
2023-10-28 16:14:53 -0700

    The hackiest workaround seen in the west

--- calhacks/pages/quiz.py
+++ calhacks/pages/quiz.py
@@ -4,13 +4,16 @@ from calhacks.state import State
 
 
 class QuizState(State):
-    questions = ['test 1', 'test 2', 'test 3']
+    questions = ['Question 1', 'Question 2', 'Question 3', 'Question 4']
     question_number: int = 0
 
     def on_page_opened(self):
         return rx.call_script("""
             window.mediaRecorder;
-            window.chunks = []
+            window.onDownloadComplete = () => {
+            
+            }
+            window.questionNumber = 1
             navigator.mediaDevices
                 .getUserMedia(
                   {
@@ -22,47 +25,48 @@ class QuizState(State):
                     window.mediaRecorder = new MediaRecorder(stream, {mimeType: 'video/webm'})
                     window.mediaRecorder.ondataavailable = (e) => {
                         const url = URL.createObjectURL(e.data);
-                        console.log(url)
                         
                         // Create a link element
                         const link = document.createElement("a");
                         
                         // Set link's href to point to the Blob URL
                         link.href = url;
-                        link.download = 'video.webm';
+                        link.download = 'question_' + questionNumber + '.webm';
+                        // I do not wish this code upon my worst enemy
+                        questionNumber++;
                         
                         link.dispatchEvent(
-                            new MouseEvent('click', { 
+                            new MouseEvent('click', {
                                 bubbles: true, 
                                 cancelable: true, 
                                 view: window 
                             })
                         );
+                        
+                        window.onDownloadComplete();
                     };
                     window.mediaRecorder.start();
                 })
         """)
 
-    def stop_recording(self):
+    def on_next_question_pressed(self):
+        self.question_number += 1
+        if self.question_number == len(self.questions):
+            return rx.call_script(
+                """
+                    window.onDownloadComplete = () => {
+                        window.location.href = "/results"
+                    }
+                    window.mediaRecorder.stop()
+                """,
+            )
         return rx.call_script(
-            """
+            f"""
                 window.mediaRecorder.stop()
+                window.mediaRecorder.start()
             """
         )
 
-    def next_question(self):
-        # currently, we will only ask a single question
-        return rx.call_script(
-            """
-                console.log("result", new Uint8Array(window.result))
-                new Uint8Array(window.result)[0]
-            """,
-            callback=QuizState.receive_chunks
-        )
-
-    def receive_chunks(self, chunks):
-        print('here')
-        # print(chunks)
 
 @rx.page()
 def quiz():
@@ -70,7 +74,7 @@ def quiz():
         rx.box(),
         rx.center(
             rx.heading(
-                "fasdfjadsklfjdslkf asdjf asdjf lkasdjflkadsa fjklasf ldskafd",
+                QuizState.questions[QuizState.question_number],
                 size="xl",
                 text_align="center",
                 max_width="60%"
@@ -79,7 +83,7 @@ def quiz():
         rx.center(
             rx.button(
                 "Next question",
-                on_click=QuizState.stop_recording,
+                on_click=QuizState.on_next_question_pressed,
             )
         ),
         rx.box(),
