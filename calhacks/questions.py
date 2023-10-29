0a14ce6505a2895d6dee7d6e142f3efc53bbbf9b
Dean Shin <deanshin365@gmail.com>
2023-10-28 21:18:33 -0700

    Behavioral and Front-end

--- calhacks/questions.py
+++ calhacks/questions.py
@@ -1,7 +1,36 @@
 role_to_questions = {
-    "Front-end": ["Question 1", "Question 2", "Question 3"],
-    "Back-end": ["Question 1", "Question 2", "Question 3"],
-    "Full-stack": ["Question 1", "Question 2", "Question 3"],
-    "AI/ML": ["Question 1", "Question 2", "Question 3"],
-    "Devops": ["Question 1", "Question 2", "Question 3"]
+    "Behavioral": [
+        "Tell me about a time you worked in a team and dealt with an issue",
+        "Tell me about a time you were under a lot of pressure at work or at school. What was going on, and how did you get through it?",
+        "Tell me about your proudest professional accomplishment",
+        "Tell me about a time you had to tell someone no.",
+        "Do you have any work habits you’d like to improve?",
+    ],
+    "Front-end": [
+        "What do you do to ensure a site is user friendly?",
+        "What techniques do you use to improve a site’s performance?",
+        "Describe three ways to decrease page load time",
+        "Suggest some ways on how to fix the browser-specific styling issue",
+        "What are pseudo-classes? Provide a few real-world use cases",
+    ],
+    "Back-end": [
+        "Question 1",
+        "Question 2",
+        "Question 3"
+    ],
+    "Full-stack": [
+        "Question 1",
+        "Question 2",
+        "Question 3"
+    ],
+    "AI/ML": [
+        "Question 1",
+        "Question 2",
+        "Question 3"
+    ],
+    "Devops": [
+        "Question 1",
+        "Question 2",
+        "Question 3"
+    ]
 }
