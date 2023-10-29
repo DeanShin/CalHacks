aefaa5286e26074230d3a61e7272fb3332d7f862
Megan <m_acosta4@u.pacific.edu>
2023-10-29 00:04:46 -0700

    making advice request

--- calhacks/gpt_client.py
+++ calhacks/gpt_client.py
@@ -20,6 +20,7 @@ class ChatGPT:
         self.user_context = user_context
         self.count = 0
         self.questions = list[str]
+        self.advice = str
 
     async def _generate_questions(self) -> dict:
         print('\t-generating questions')
@@ -33,7 +34,7 @@ class ChatGPT:
             messages=[
                 {
                     "role": "system",
-                    "content": f"You are a a helpful interviewer that will help me prep for my {self.user_context} interview",
+                    "content": f"You are a helpful interviewer that will help me prep for my {self.user_context} interview",
                 },
                 {"role": "user", "content": f"{prompt}"},
             ],
@@ -55,10 +56,43 @@ class ChatGPT:
         questions = await self._generate_questions()
         self.questions = self._parse_questions(questions)
 
+    async def _generate_advice(self) -> str:
+        print('\t-generating advice')
+        prompt = f"""I was asked as an interviewee 'What does the bash command 'chmod' do?' And I responded with 'chmod will change the umm
+        the mode of the file. You have several different options to change and you can read and execute. uh.. yeah'. What advice do you have for me?"""
+
+        # prompt chatgpt
+        advice = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {
+                        "role": "system",
+                        "content": f"You will act as a coding interview coach that gives concise advice."
+                    },
+                    {"role": "user", "content": f"{prompt}"},
+                ],
+        )
+        return advice
+
+    def _parse_advice(self, advice_raw: dict) -> list[str]:
+        print('\t-parsing advice')
+        raw = advice_raw["choices"][0]["message"]["content"]
+        with open('advice_raw.txt', 'a+') as f:
+            f.write(raw)
+
+        return raw
+
+    async def get_advice(self) -> None:
+        print('\t-get advice')
+        advice = await self._generate_advice()
+        self.advice = self._parse_advice(advice)
+
 
 async def main():
-    client = ChatGPT("algorithms and their time complexities")
-    questions = await client.set_questions()
-    print(client.questions)
+    client = ChatGPT("Common Linux CLI commands")
+    #questions = await client.set_questions()
+    #print(client.questions)
+    advice = await client.get_advice()
+    print(client.advice)
 if __name__ == "__main__":
     asyncio.run(main())
