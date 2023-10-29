e15b3c982337c8b3ba711c3f1a7128825b585363
Dean Shin <deanshin365@gmail.com>
2023-10-28 22:43:35 -0700

    Layout for results page

--- calhacks/pages/results.py
+++ calhacks/pages/results.py
@@ -1,11 +1,14 @@
 from calhacks.state import State
 
 import reflex as rx
+import re
+
 
 class ResultsState(State):
     """The app state."""
     # The images to show.
     videos: list[str]
+    has_videos: bool = False
     is_uploaded: bool = False
 
     async def handle_upload(self, files: list[rx.UploadFile]):
@@ -25,11 +28,49 @@ class ResultsState(State):
 
             # Update the videos var.
             self.videos.append(file.filename)
+        self.has_videos = True
         # TODO: uncommend below to show results_view() component when video file is uploaded
         # self.is_uploaded = True
         
 
 color = '' # TODO: make it pretty
+
+
+def category_heading(text) -> rx.Component:
+    return rx.heading(
+        rx.span(
+            text,
+            display="content-block",
+            padding=".1em .1em",
+            border_radius="1000px",
+            background="#BFDBF7"
+        ),
+        margin_top="16px",
+        margin_bottom="16px"
+    )
+
+def video_and_report(video) -> rx.Component:
+    return rx.card(
+        rx.grid(
+            rx.video(url=video),
+            rx.box(
+                category_heading("Content"),
+                rx.text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
+                category_heading("Presentation"),
+                rx.text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
+                category_heading("Key Moments"),
+                rx.list(
+                    rx.list_item("00:05 -- High Nervousness"),
+                    rx.list_item("00:20 -- High Confidence"),
+                    rx.list_item("03:40 -- High Rizz"),
+                )
+            ),
+            grid_template_columns="1fr 1fr",
+            align_items="center",
+            gap="32px"
+        )
+    )
+
 
 def upload_view() -> rx.Component:
     """
@@ -59,15 +100,25 @@ def upload_view() -> rx.Component:
         rx.button(
             "Upload",
             on_click=lambda: ResultsState.handle_upload(
-                rx.upload_files()
+                rx.upload_files(),
             ),
         ),
         rx.button(
             "Clear",
             on_click=rx.clear_selected_files,
         ),
-        rx.foreach(
-            ResultsState.videos, lambda videos: rx.box(rx.heading('video!'), rx.video(url=videos))
+        rx.cond(
+            ResultsState.has_videos,
+            rx.flex(
+                rx.foreach(
+                    ResultsState.videos,
+                    lambda video: video_and_report(video)
+                ),
+                background_color="#E1E5F2",
+                flex_direction="column",
+                padding="32px",
+                gap="32px"
+            ),
         ),
         padding="5em",
     )
